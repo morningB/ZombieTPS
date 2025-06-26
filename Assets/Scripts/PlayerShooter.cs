cbb3a1797d7f89d9c85c46ef6903f913b696e0a6
morningB <yjw001205@gmail.com>
2025-06-26 10:15:32 +0900

    feat: 플레이어 총 발사 기능 추가

--- Assets/Scripts/PlayerShooter.cs
+++ Assets/Scripts/PlayerShooter.cs
@@ -21,7 +21,7 @@ public class PlayerShooter : MonoBehaviour {
         // 슈터가 활성화될 때 총도 함께 활성화
         gun.gameObject.SetActive(true);
     }
-    
+
     private void OnDisable() {
         // 슈터가 비활성화될 때 총도 함께 비활성화
         gun.gameObject.SetActive(false);
@@ -29,6 +29,23 @@ public class PlayerShooter : MonoBehaviour {
 
     private void Update() {
         // 입력을 감지하고 총 발사하거나 재장전
+        if (playerInput.fire)
+        {
+            // 발사 입력 감지시 총 발사
+            gun.Fire();
+        }
+        else if (playerInput.reload)
+        {
+            // 재장전 입력 감지시 재장전
+            if (gun.Reload())
+            {
+                // 재장전 성공시에만 재장전 애니메이션 재생
+                playerAnimator.SetTrigger("Reload");
+            }
+        }
+
+        // 남은 탄약 UI를 갱신
+        UpdateUI();
     }
 
     // 탄약 UI 갱신
@@ -42,6 +59,22 @@ public class PlayerShooter : MonoBehaviour {
 
     // 애니메이터의 IK 갱신
     private void OnAnimatorIK(int layerIndex) {
-        
+        // 총의 기준점 gunPivot을 3D 모델의 오른쪽 팔꿈치 위치로 이동
+        gunPivot.position =
+            playerAnimator.GetIKHintPosition(AvatarIKHint.RightElbow);
+
+        // IK를 사용하여 왼손의 위치와 회전을 총의 오른쪽 손잡이에 맞춘다
+        playerAnimator.SetIKPositionWeight(AvatarIKGoal.LeftHand, 1.0f);
+        playerAnimator.SetIKRotationWeight(AvatarIKGoal.LeftHand, 1.0f);
+
+        playerAnimator.SetIKPosition(AvatarIKGoal.LeftHand, leftHandMount.position);
+        playerAnimator.SetIKRotation(AvatarIKGoal.LeftHand, leftHandMount.rotation);
+
+        // IK를 사용하여 오른손의 위치와 회전을 총의 오른쪽 손잡이에 맞춘다
+        playerAnimator.SetIKPositionWeight(AvatarIKGoal.RightHand, 1.0f);
+        playerAnimator.SetIKRotationWeight(AvatarIKGoal.RightHand, 1.0f);
+
+        playerAnimator.SetIKPosition(AvatarIKGoal.RightHand, rightHandMount.position);
+        playerAnimator.SetIKRotation(AvatarIKGoal.RightHand, rightHandMount.rotation);
     }
 }
